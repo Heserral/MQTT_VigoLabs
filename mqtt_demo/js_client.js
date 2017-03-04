a72b9e022c1dd8a979186dabc08f85290661e5d1
Heserral <heserral@gmail.com>
2017-03-04 01:06:28 +0100

    first demo

---
+++ mqtt_demo/js_client.js
@@ -0,0 +1,17 @@
+/*
+https://github.com/mqttjs/MQTT.js
+test.mosquitto.org
+*/
+
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
+
+client.on('connect', function () {
+  client.subscribe('presence')
+  client.publish('presence', 'Hello mqtt')
+})
+
+client.on('message', function (topic, message) {
+  console.log(message.toString())
+  //client.end()
+})
