4602f0d546f429a8c8794a655f14de5c38e9b702
Heserral <heserral@gmail.com>
2017-03-04 13:48:42 +0100

    Added angular MQTT wrapper

--- mqtt_demo/ws_motor/app.js
+++ mqtt_demo/ws_motor/app.js
@@ -1,3 +1,18 @@
+//https://github.com/shellus/angular-MQTT
+
+var app = angular.module('myapp', ['ngMQTT']);
+app.config(['MQTTProvider', function (MQTTProvider) {
+    MQTTProvider.setHref('ws://cv.endaosi.com:18585');
+}]);
+app.controller('indexCtrl', ['$scope', 'MQTTService', function ($scope, MQTTService) {
+    MQTTService.on('hello', function (data) {
+        alert(data)
+    });
+    MQTTService.send('hello', 'word');
+    MQTTService.send('hello', 'word1');
+    MQTTService.send('hello', 'word2');
+}]);
+
 $(function () { 
     var myChart = Highcharts.chart('container', {
         chart: {
